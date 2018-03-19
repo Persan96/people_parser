7cd8806927e17fc93c853e21ea632eacf7e1697e
Persan96 <per.sandgren@protonmail.ch>
2018-03-19 16:13:42 +0100

    Restructured code for readability, added support for failed input pipes

--- pypeople_parser.py
+++ pypeople_parser.py
@@ -1,6 +1,6 @@
 import sys
 
-first_argument, second_argument, third_argument, forth_argument = 0, 1, 2, 3
+first, second, third, forth = 0, 1, 2, 3
 persons, families, phones, addresses = "P", "F", "T", "A"
 file = open(sys.argv[1], "r")
 person_found = False
@@ -8,26 +8,32 @@ family_found = False
 output = [ "<people>\n\t" ]
 for i, line in enumerate(file):
     line.replace('\n', '').replace('\r', '')
-    line_arguments = line.split("|")
-    if (line_arguments[first_argument] == persons):
+    lines = line.split("|")
+    arguments = []
+    for argument in lines:
+        arguments.append(argument)
+    for i in range(len(lines), 4):
+        arguments.append("NO-INPUT")
+
+    if (arguments[first] == persons):
         if(person_found == False):
             person_found = True
-            output.append("<person><firstname>" + line_arguments[second_argument] + "</firstname><lastname>" + line_arguments[third_argument] + "</lastname>\n\t\t")
+            output.append("<person><firstname>" + arguments[second] + "</firstname><lastname>" + arguments[third] + "</lastname>\n\t\t")
         else:
             if(family_found == True):
                 output.append("</family>\n")
                 family_found = False
-            output.append("</person>\n\t\t<person><firstname>" + line_arguments[second_argument] + "</firstname><lastname>" + line_arguments[third_argument] + "</lastname>\n\t\t")
-    if (line_arguments[first_argument] == families):
+            output.append("</person>\n\t\t<person><firstname>" + arguments[second] + "</firstname><lastname>" + arguments[third] + "</lastname>\n\t\t")
+    if (arguments[first] == families):
         if(family_found == False):
             family_found = True
-            output.append("<family><name>" + line_arguments[second_argument] + "</name><birth>" + line_arguments[third_argument] + "</birth>\n\t\t")
+            output.append("<family><name>" + arguments[second] + "</name><birth>" + arguments[third] + "</birth>\n\t\t")
         else:
-            output.append("</family>\n\t\t<family><name>" + line_arguments[second_argument] + "</name><birth>" + line_arguments[third_argument] + "</birth>")
-    if (line_arguments[first_argument] == addresses):
-        output.append("<address><street>" + line_arguments[second_argument] + "</street><city>" + line_arguments[third_argument] + "</city><zip>" + line_arguments[forth_argument] + "</zip></address>\n\t\t")
-    if (line_arguments[first_argument] == phones):
-            output.append("<phone><mobile>" + line_arguments[second_argument] + "</mobile><landline>" + line_arguments[third_argument] + "</landline></phone>\n\t\t")
+            output.append("</family>\n\t\t<family><name>" + arguments[second] + "</name><birth>" + arguments[third] + "</birth>")
+    if (arguments[first] == addresses):
+        output.append("<address><street>" + arguments[second] + "</street><city>" + arguments[third] + "</city><zip>" + arguments[forth] + "</zip></address>\n\t\t")
+    if (arguments[first] == phones):
+            output.append("<phone><mobile>" + arguments[second] + "</mobile><landline>" + arguments[third] + "</landline></phone>\n\t\t")
 if(family_found == True):
     output.append("</family>\n\t\t")
 output.append("</person>\n</people>")
